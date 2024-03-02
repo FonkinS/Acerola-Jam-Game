20c4ca349bba178d1ad61fe7bf694accbd5d6290
FonkinS <85748962+FonkinS@users.noreply.github.com>
2024-03-02 14:03:58 +0100

    Added Basic Player Sprite, as well as the feature of dropping through platforms

--- Source/Scripts/Player.gd
+++ Source/Scripts/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const jumpHeight := 128
+const jumpHeight := 90
 const jumpTime := 0.25
-const moveSpeed := 400
+const moveSpeed := 300
 const acceleration := 0.5
 const deceleration := 0.5
 
@@ -13,6 +13,8 @@ var velocity : Vector2
 
 var coyoteTimer := 0.0
 const coyoteTime := 0.3
+
+var is_jumping := false
 
 func _ready():
 	GRAVITY = (2 * jumpHeight) / pow(jumpTime, 2)
@@ -28,7 +30,15 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, h_input * moveSpeed, 1 / acceleration * delta * 4.75)
 	
+	if not is_jumping:
+		if h_input == 0:
+			$AnimationPlayer.play("Idle")
+		else:
+			$AnimationPlayer.play("Walk")
+	
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyoteTimer > 0):
+		is_jumping = true
+		$AnimationPlayer.play("Jump")
 		velocity.y = -JUMP_STRENGTH
 	
 	if is_on_floor():
@@ -36,4 +46,13 @@ func _physics_process(delta):
 	else:
 		coyoteTimer -= delta
 	
+	if Input.is_action_just_pressed("drop"):
+		set_collision_mask_bit(3, false)
+		yield(get_tree().create_timer(0.1), "timeout")
+		set_collision_mask_bit(3, true)
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
+
+
+func _on_jump_finish():
+	is_jumping = false
